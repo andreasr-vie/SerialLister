43a364f625f22549959432e54dcc236a593573cd
andreasresch <andreasresch@A-LENOVOT14>
2022-10-02 17:23:08 +0200

    Add project files.

---
+++ SerialLister/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SerialLister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(375, 112);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 180);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Port List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SerialLister/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+using Microsoft.Win32;
+using System.Security.Principal;
+using System.Collections;
+
+namespace SerialLister
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSerialPorts();
+        }
+
+        public void GetSerialPorts()
+        {
+            listBox1.Items.Clear();
+            var list = new List<KeyValuePair<int, string>>();
+            int i = 0;
+
+            using (ManagementClass i_Entity = new ManagementClass("Win32_PnPEntity"))
+            {
+                foreach (ManagementObject i_Inst in i_Entity.GetInstances())
+                { 
+                    Object o_Guid = i_Inst.GetPropertyValue("ClassGuid");
+                    if (o_Guid == null || o_Guid.ToString().ToUpper() != "{4D36E978-E325-11CE-BFC1-08002BE10318}")
+                        continue; // Skip all devices except device class "PORTS"
+
+                    String s_Caption = i_Inst.GetPropertyValue("Caption").ToString();
+                    String s_Manufact = i_Inst.GetPropertyValue("Manufacturer").ToString();
+                    String s_DeviceID = i_Inst.GetPropertyValue("PnpDeviceID").ToString();
+                    String s_RegPath = "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Enum\\" + s_DeviceID + "\\Device Parameters";
+                    String s_PortName = Registry.GetValue(s_RegPath, "PortName", "").ToString();
+
+                    int s32_Pos = s_Caption.IndexOf(" (COM");
+                    if (s32_Pos > 0) // remove COM port from description
+                        s_Caption = s_Caption.Substring(0, s32_Pos);
+
+                    var sp = s_PortName + "\t  " + s_Caption;
+                    int spn = Convert.ToInt32(s_PortName.Remove(0, 3));
+
+                    list.Add(new KeyValuePair<int, string>(spn, sp));
+                    i++;
+                }
+            }
+            list = list.OrderBy(kvp => kvp.Key).ToList();
+            foreach (var element in list)
+            {
+                listBox1.Items.Add(element.Value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetSerialPorts();
+        }
+    }
+}
